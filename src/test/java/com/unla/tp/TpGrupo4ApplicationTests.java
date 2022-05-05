ab70c29e04191699d2d653dd4c70520173409ab8
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-05-05 18:04:46 -0300

    first commit

---
+++ src/test/java/com/unla/tp/TpGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGrupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
