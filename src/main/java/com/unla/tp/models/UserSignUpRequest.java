eee1de2bce40648d63a3e1dde7ae3dba612ed5a3
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-05-13 14:19:28 -0300

    Added model used at register of new user

---
+++ src/main/java/com/unla/tp/models/UserSignUpRequest.java
@@ -0,0 +1,15 @@
+package com.unla.tp.models;
+
+import lombok.Data;
+
+@Data
+public class UserSignUpRequest {
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String password;
+    private String email;
+    private String tipoDocumento;
+    private Double nroDocumento;
+    private int roleId; // ver
+}
