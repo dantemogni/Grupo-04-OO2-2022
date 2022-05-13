4f414d82187cd88a3a4ad21d860b6c2eca31e1aa
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-05-13 14:17:22 -0300

    Added basic controller classes for inital workflow

---
+++ src/main/java/com/unla/tp/controllers/helpers/ViewRouteHelper.java
@@ -0,0 +1,16 @@
+package com.unla.tp.controllers.helpers;
+
+public class ViewRouteHelper {
+    private ViewRouteHelper() {
+        throw new IllegalStateException("Helper class");
+    }
+
+    // HOME
+    public static final String INDEX = "home/index";
+    public static final String HELLO = "home/hello";
+
+    // USER
+    public static final String USER_LOGIN = "user/login";
+    public static final String USER_LOGOUT = "user/logout";
+    public static final String USER_REGISTRATION = "user/register";
+}
