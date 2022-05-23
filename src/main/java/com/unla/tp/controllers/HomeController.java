ab719f80bc98737ffec78f634fa4d8b3b788e538
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-05-23 19:51:33 -0300

    New: register view. Various Fixes

--- src/main/java/com/unla/tp/controllers/HomeController.java
+++ src/main/java/com/unla/tp/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.unla.tp.controllers;
 
 import com.unla.tp.controllers.helpers.ViewRouteHelper;
-import com.unla.tp.entities.User;
+import com.unla.tp.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping(value = { "/index", "/" })
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.INDEX);
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userDetails = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        modelAndView.addObject("username", user.getUsername());
-        modelAndView.addObject("nombre", user.getNombre());
-        modelAndView.addObject("apellido", user.getApellido());
+        com.unla.tp.entities.User userEntity = userService.getByUsername(userDetails.getUsername());
+
+        modelAndView.addObject("username", userDetails.getUsername());
+        modelAndView.addObject("nombre", userEntity.getNombre());
+        modelAndView.addObject("apellido", userEntity.getApellido());
+
+        modelAndView.addObject("userRole", userDetails.getAuthorities().stream().toArray()[0].toString());
 
         return modelAndView;
     }
--- src/main/java/com/unla/tp/controllers/UserController.java
+++ src/main/java/com/unla/tp/controllers/UserController.java
@@ -1,12 +1,18 @@
 package com.unla.tp.controllers;
 
+import javax.validation.Valid;
+
 import com.unla.tp.controllers.helpers.ViewRouteHelper;
 import com.unla.tp.models.UserSignUpRequest;
+import com.unla.tp.models.validator.UserSignUpRequestValidator;
 import com.unla.tp.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,10 +25,31 @@ public class UserController {
         return new ModelAndView(ViewRouteHelper.USER_LOGIN);
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping(value = "/register")
     public ModelAndView registration() {
         ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.USER_REGISTRATION);
         modelAndView.addObject("userSignUpRequest", new UserSignUpRequest());
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/register")
+    public ModelAndView createNewUser(@Valid UserSignUpRequest user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.USER_REGISTRATION);
+
+        modelAndView.addObject("userSignUpRequest", user);
+
+        UserSignUpRequestValidator uv = new UserSignUpRequestValidator(userService);
+        uv.validate(user, bindingResult);
+
+        // checks empty value errors
+        if (bindingResult.hasErrors()) {
+            return modelAndView;
+        }
+
+        userService.createUser(user);
+        modelAndView.addObject("successMessage", "Registro exitoso");
+
         return modelAndView;
     }
 
