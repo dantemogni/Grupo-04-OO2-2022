a83e65dab7ea9649053a96bb795fd8ea7ca295c4
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-05-23 19:52:45 -0300

    Fixes regarding roles authorities

--- src/main/java/com/unla/tp/services/implementation/UserServiceImpl.java
+++ src/main/java/com/unla/tp/services/implementation/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.unla.tp.services.implementation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import com.unla.tp.entities.User;
 import com.unla.tp.models.UserSignUpRequest;
 import com.unla.tp.respositories.RoleRepository;
@@ -7,6 +10,8 @@ import com.unla.tp.respositories.UserRepository;
 import com.unla.tp.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,27 +40,31 @@ public class UserServiceImpl implements UserService {
         user.setPassword(bCryptPasswordEncoder.encode(userRequest.getPassword()));
         user.setUsername(userRequest.getUsername());
         user.setRole(roleRepository.getById(userRequest.getRoleId()));
+        user.setEnabled(true);
 
         return userRepository.save(user);
     }
 
-    // private User buildUser(UserSignUpRequest user) {
-    // return User.builder()
-    // .nombre(user.getNombre())
-    // .apellido(user.getApellido())
-    // .email(user.getEmail())
-    // .nroDocumento(user.getNroDocumento())
-    // .tipoDocumento(user.getTipoDocumento())
-    // .username(user.getUsername())
-    // .password(bCryptPasswordEncoder.encode(user.getPassword())
-    // .build();
-    // }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository
-                .findOneByUsername(username)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException("User with username - " + username + ", not found"));
+        User user = userRepository.findOneByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username - " + username + ", not found"));
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(getAuthority(user));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), true, true, true, authorities);
     }
+
+    private GrantedAuthority getAuthority(User model) {
+        return new SimpleGrantedAuthority(model.getRole().getAuthority());
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        return userRepository.findOneByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username - " + username + ", not found"));
+    }
+
 }
