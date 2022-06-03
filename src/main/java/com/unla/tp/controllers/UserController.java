843f4eb1ead05fbc786ca31c94947c8a35cee5e2
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-03 13:51:34 -0300

    New: PDF export controller

--- src/main/java/com/unla/tp/controllers/UserController.java
+++ src/main/java/com/unla/tp/controllers/UserController.java
@@ -136,4 +136,21 @@ public class UserController {
         return modelAndView;
     }
 
+    @GetMapping("/users/export/pdf")
+    public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<User> listUsers = userService.getAll();
+
+        UserPDFExporter exporter = new UserPDFExporter(listUsers);
+        exporter.export(response);
+
+    }
+
 }
