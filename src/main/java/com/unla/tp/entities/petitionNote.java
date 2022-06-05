1dfc43fe5858808f7bace056edc2c2e042f6f8c0
admin <imnordi@gmail.com>
2022-06-05 14:14:34 -0300

    petitionNote entitie

---
+++ src/main/java/com/unla/tp/entities/petitionNote.java
@@ -0,0 +1,49 @@
+package com.unla.tp.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class petitionNote {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private LocalDate fecha;
+
+    @Column
+    private char turno;
+
+    @Column
+    private int cantEstudiantes;
+
+    @Column
+    private String observaciones;
+
+    @ManyToOne
+    @JoinColumn(name = "idMateria")
+    private Materia materia;
+
+    @ManyToOne
+    @JoinColumn(name = "idCarrera")
+    private Carrera carrera;
+
+}
