c27b277c1e785ce7de6d8fb0a3f6a36a48d4ebac
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-06 23:49:13 -0300

    New: Espacio controller

---
+++ src/main/java/com/unla/tp/controllers/EspacioController.java
@@ -0,0 +1,31 @@
+package com.unla.tp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.unla.tp.controllers.helpers.ViewRouteHelper;
+import com.unla.tp.services.IEspacioService;
+
+@Controller
+@RequestMapping("/space")
+public class EspacioController {
+    @Autowired
+    private IEspacioService espacioService;
+
+    @GetMapping("/list")
+    public ModelAndView lstEspacios() {
+
+        ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.SPACE_LST);
+        // try {
+        // espacioService.inicializarEspacios(3, 2022);
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        modelAndView.addObject("spaces", espacioService.getAll());
+
+        return modelAndView;
+    }
+}
