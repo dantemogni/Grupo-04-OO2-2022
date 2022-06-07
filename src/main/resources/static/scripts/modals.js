b6ce4f3fed72f845a43b548bfb138d920d95dda2
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-07 00:00:09 -0300

    Classroom details modal

--- src/main/resources/static/scripts/modals.js
+++ src/main/resources/static/scripts/modals.js
@@ -1,4 +1,42 @@
 
+
+  $("#traditionalDetailsModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+  
+    var bancos = button.data("bancos");
+    var pizarron = button.data("pizarron");
+    var proyector = button.data("proyector");
+
+    var modal = $(this);
+  
+
+    if(proyector){
+      proyector = "✅ Si"
+    } else {
+      proyector = "❌ No"
+    }
+
+    modal.find("#classroomBancos").text(bancos);
+    modal.find("#classroomPizarron").text(pizarron);
+    modal.find("#classroomProyector").text(proyector);
+
+  
+  });
+
+  $("#labDetailsModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+  
+    var pc = button.data("pc");
+    var sillas = button.data("sillas");
+
+    var modal = $(this);
+  
+    modal.find("#classroomPc").text(pc);
+    modal.find("#classroomSillas").text(sillas);
+
+  
+  });
+
   $("#deleteModal").on("show.bs.modal", function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
   
