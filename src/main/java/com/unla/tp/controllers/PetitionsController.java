75e8dd82cd0192269429760cf112b56008bb6db9
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-06 23:58:47 -0300

    PostMapping for petitionsNote

--- src/main/java/com/unla/tp/controllers/PetitionsController.java
+++ src/main/java/com/unla/tp/controllers/PetitionsController.java
@@ -9,32 +9,49 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.unla.tp.controllers.helpers.ViewRouteHelper;
 import com.unla.tp.models.Petition;
-
+import com.unla.tp.services.IOrderNoteService;
 
 @Controller
 public class PetitionsController {
+    @Autowired
+    private IOrderNoteService orderNoteService;
 
-
-    //REDIRIGE A LAS PETICIONES HECHAS
+    // REDIRIGE A LAS PETICIONES HECHAS
     @Secured("ROLE_ADMIN")
     @GetMapping("/manOrderNotes")
-    public ModelAndView petitions(){
+    public ModelAndView petitions() {
 
         ModelAndView mV = new ModelAndView(ViewRouteHelper.PETITIONS_LST);
-        
+
         return mV;
     }
 
-    //REDIRIGE A NUEVAS NOTAS DE PEDIDO
+    // REDIRIGE A NUEVAS NOTAS DE PEDIDO
     @Secured("ROLE_PROFESOR")
     @GetMapping("/petitionNote")
-    public ModelAndView newPetition(){
+    public ModelAndView newPetition() {
 
         ModelAndView mV = new ModelAndView(ViewRouteHelper.NEW_PETITION);
         mV.addObject("petition", new Petition());
         return mV;
     }
 
-    // @PostMapping("/petitionNote")
-    
+    @PostMapping("/petitionNote")
+    public ModelAndView processPetition(Petition petition) {
+        ModelAndView mV = new ModelAndView(ViewRouteHelper.NEW_PETITION);
+        mV.addObject("petition", new Petition());
+
+        // UserRequestValidator uv = new UserRequestValidator(userService);
+        // uv.validate(user, bindingResult);
+
+        // checks empty value errors
+        // if (bindingResult.hasErrors()) {
+        // return modelAndView;
+        // }
+
+        orderNoteService.create(petition);
+        mV.addObject("successMessage", "Registro exitoso");
+
+        return mV;
+    }
 }
