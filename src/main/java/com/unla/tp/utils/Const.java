c926131150dc6962f82531ea072240325d8c6310
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-07 02:31:51 -0300

    New: added constants

---
+++ src/main/java/com/unla/tp/utils/Const.java
@@ -0,0 +1,8 @@
+package com.unla.tp.utils;
+
+public class Const {
+    public static final String STATUS_PROCESSING = "enProceso";
+    public static final String STATUS_DECLINE = "rechazado";
+    public static final String STATUS_OK = "aceptada";
+
+}
