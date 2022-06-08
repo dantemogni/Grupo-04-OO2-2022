2cf91fa05853fc40273a358613d3700a82d75455
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-07 22:21:00 -0300

    new: departamento entity

---
+++ src/main/java/com/unla/tp/entities/Departamento.java
@@ -0,0 +1,28 @@
+package com.unla.tp.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Departamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String departamento;
+
+}
