cb300ab30778220ee4ab64b08785e09f3a10cadd
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-07 22:23:58 -0300

    completed Materia entity

--- src/main/java/com/unla/tp/entities/Carrera.java
+++ src/main/java/com/unla/tp/entities/Carrera.java
@@ -1,11 +1,34 @@
 package com.unla.tp.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Builder
 public class Carrera {
 
     @Id
-    private int idCarrera;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String carrera;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_departamento")
+    private Departamento departamento;
 }
