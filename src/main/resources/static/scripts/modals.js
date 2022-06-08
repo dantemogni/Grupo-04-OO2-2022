e3bf7dd01cf9457df0ea01f983f6522f68c8684b
Dante Mogni <dantejoaquin.mogni@gmail.com>
2022-06-07 22:30:25 -0300

    views updated

--- src/main/resources/static/scripts/modals.js
+++ src/main/resources/static/scripts/modals.js
@@ -37,6 +37,56 @@
   
   });
 
+  // MODALS PETITION NOTE ACTIONS
+  $("#traditionalPetitionDetailsModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+  
+    var bancos = button.data("bancos");
+    var pizarron = button.data("pizarron");
+    var proyector = button.data("proyector");
+    var petitonId = button.data("petitionid");
+    var spaceId = button.data("spaceid");
+
+    console.log(spaceId)
+
+    var acceptLink = "/petitions/accept/"+petitonId+"/for-space/"+spaceId
+
+    var modal = $(this);
+  
+
+    if(proyector){
+      proyector = "✅ Si"
+    } else {
+      proyector = "❌ No"
+    }
+
+    modal.find("#classroomBancos").text(bancos);
+    modal.find("#classroomPizarron").text(pizarron);
+    modal.find("#classroomProyector").text(proyector);
+
+    modal.find("#acceptLink").attr("action", acceptLink);
+
+  });
+
+  $("#labDetailsPetitionModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+  
+    var pc = button.data("pc");
+    var sillas = button.data("sillas");
+    var petitonId = button.data("petitionid");
+    var spaceId = button.data("spaceid");
+
+    var acceptLink = "/petitions/accept/"+petitonId+"/for-space/"+spaceId
+
+    var modal = $(this);
+  
+    modal.find("#classroomPc").text(pc);
+    modal.find("#classroomSillas").text(sillas);
+ 
+    modal.find("#acceptLink").attr("action", acceptLink);
+  
+  });
+
   $("#deleteModal").on("show.bs.modal", function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
   
